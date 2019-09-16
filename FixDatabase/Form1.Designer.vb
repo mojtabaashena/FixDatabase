4bcbc2179bfdb9c464f03a3e65ef2904bfa099e4
ashena <ashena@SOFT-PC>
2019-09-16 09:07:51 +0430

    Add project files.

---
+++ FixDatabase/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtQuery = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtConnectionString = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.Location = New System.Drawing.Point(319, 212)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Execute"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtQuery
+        '
+        Me.txtQuery.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtQuery.Location = New System.Drawing.Point(13, 58)
+        Me.txtQuery.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtQuery.Multiline = True
+        Me.txtQuery.Name = "txtQuery"
+        Me.txtQuery.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtQuery.Size = New System.Drawing.Size(706, 144)
+        Me.txtQuery.TabIndex = 1
+        Me.txtQuery.Text = "SELECT '' AS [FIXDatabase]" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "GO" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ALTER TABLE Factor@Year@ ALTER COLUMN LouderNO Te" &
+    "xt(250)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "GO" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "UPDATE  Factor@Year@ SET LouderNO='' WHERE   LouderNO IS NULL" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "GO" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) &
+    ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(13, 246)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(706, 223)
+        Me.TextBox1.TabIndex = 2
+        '
+        'txtConnectionString
+        '
+        Me.txtConnectionString.Location = New System.Drawing.Point(13, 13)
+        Me.txtConnectionString.Name = "txtConnectionString"
+        Me.txtConnectionString.Size = New System.Drawing.Size(706, 22)
+        Me.txtConnectionString.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(732, 487)
+        Me.Controls.Add(Me.txtConnectionString)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtQuery)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtQuery As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtConnectionString As TextBox
+End Class
---
+++ FixDatabase/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Try
+        '    Dim oleDbDataAdapter As OleDbDataAdapter = New OleDbDataAdapter(txtQuery.Text, "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + Application.StartupPath + "\DBHesabPardaz.dat';Jet OLEDB:Database Password='D5oas$9X[21}U1)8'")
+        '    Dim dataSet As DataSet = New DataSet()
+        '    oleDbDataAdapter.Fill(dataSet)
+        '    MessageBox.Show("Completed")
+        'Catch expr_3C As Exception
+        '    Dim ex As Exception = expr_3C
+        '    MessageBox.Show(ex.Message)
+        'End Try
+        Dim strCn As String = txtConnectionString.Text
+
+        If IO.File.Exists(Application.StartupPath + "\cn.dll") Then
+            strCn = IO.File.ReadAllText(Application.StartupPath + "\cn.dll")
+            txtConnectionString.Text = "read from file"
+        End If
+
+        Dim connection As New OleDbConnection(strCn)
+        Dim cmd As New OleDbCommand("", connection)
+
+        connection.Open()
+        Dim strErrorReport As String = ""
+        Dim intCount As Integer = 0
+
+        For Each str As String In txtQuery.Text.Split(New String() {vbCr & vbLf & "GO" & vbCr & vbLf}, StringSplitOptions.RemoveEmptyEntries)
+            Try
+                'If str.Contains("DROP VIEW viwKarsh") Then
+                '    Debug.WriteLine(str)
+                'End If
+                If str.Contains("@Year@") Then
+                    Try
+                        cmd.CommandText = str.Replace("@Year@", "_Year")
+                        cmd.ExecuteNonQuery()
+                    Catch ex As Exception
+                        strErrorReport += intCount.ToString() & ": [ " & cmd.CommandText.Substring(0, 30) & " ]" & ex.Message & vbCr & vbLf
+                    End Try
+                End If
+
+                cmd.CommandText = str.Replace("@Year@", "")
+                strErrorReport += intCount.ToString() & ": (" & cmd.ExecuteNonQuery().ToString() & " rows affected)" & vbCr & vbLf
+            Catch ex As Exception
+                strErrorReport += intCount.ToString() & ": [ " & str.Substring(0, If(str.Length < 31, str.Length - 1, 30)) & " ]" & ex.Message & vbCr & vbLf
+            End Try
+            intCount += 1
+        Next
+
+        TextBox1.Text = strErrorReport
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If IO.File.Exists(Application.StartupPath + "\Query.txt") Then
+            txtQuery.Text = IO.File.ReadAllText(Application.StartupPath + "\Query.txt")
+        End If
+    End Sub
+End Class
